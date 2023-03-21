31c6882463282fac0a54186a1f1ae2797d3c7935
Johnydavid <dropin2johny@gmail.com>
2023-03-21 22:34:20 +0530

    slideshow html

---
+++ script.js
@@ -0,0 +1,28 @@
+// var slider_img = document.querySelector('.slider-img');
+var images = document.getElementsByTagName("figure");
+var i = 0;
+
+
+function prev(){
+    for(i=0; i<images.length; i++){
+       console.log(images);
+    }
+    
+
+        // console.log(images.length)
+    
+}
+
+    	
+
+
+// function next(){
+// 	if(i >= images.length-1) i = -1;
+// 	i++;
+// 	return setImg();			 
+// }
+
+// function setImg(){
+// 	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+// 
